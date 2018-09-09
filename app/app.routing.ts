f53bb15c432b4f07e2c8a0e545ce0564e161bdac
Rafael Nuguid <raf@Rafaels-MacBook-Pro-2.local>
2018-09-08 17:20:26 -0700

    Update calendars, change layout a bit as requested.. add some pdf downloads

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -3,7 +3,6 @@ import { ModuleWithProviders } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HoracioComponent } from './horacio.component';
 import { RosannaComponent } from './rosanna.component';
-import { AlumniComponent } from './alumni.component';
 import { EventsComponent } from './events.component';
 import { AdmissionComponent } from './admission.component';
 import { HoracioCalendarComponent } from './horacio-calendar.component';
@@ -18,13 +17,13 @@ const my_routes: Routes = [
     {path: 'horacio', component: HoracioComponent },
     {path: 'rosanna', component: RosannaComponent },
 	{path: 'events', component: EventsComponent },
-	{path: 'alumni', component: AlumniComponent },
 	{path: 'horacio-calendar', component: HoracioCalendarComponent },
 	{path: 'rosanna-calendar', component: RosannaCalendarComponent },
 //	{path: 'under-construction', component: UnderConstructionComponent },
 	{path: 'under-construction', component: UnderConstructionComponent },
 	{path: 'snow-day-policy', component: SnowDayPolicyComponent },
-	{path: 'admin', component: AdminComponent }
+	{path: 'admin', component: AdminComponent },
+    {path: 'test', redirectTo: '/assets/pdf/test.pdf'}
 //	{path: 'current-students', component: CurrentStudentsComponent },
 ]
 
