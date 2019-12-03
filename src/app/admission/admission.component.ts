8435957662930077538c818f75997d1048560f52
Rafael Nuguid <raf.nuguid@gmail.com>
2019-12-02 22:43:03 -0800

    Add bootstrap 4.4.1 support.  So also switch to using sassy.  Start adding form validators.

--- src/app/admission/admission.component.ts
+++ src/app/admission/admission.component.ts
@@ -3,12 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admission',
   templateUrl: './admission.component.html',
-  //template: '',
   //styleUrls: ['./admission.component.css']
 })
 export class AdmissionComponent implements OnInit {
-
-  constructor() { }
+  onSubmit() { }
 
   ngOnInit() {
   }
